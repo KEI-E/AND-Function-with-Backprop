6b4fa3d48a3f93584ee849df4d934271927174c0
kaye.engbino <engbinop@gmail.com>
2022-03-01 18:07:02 +0800

    OR Function
    codes are commented out since this app is for AND

---
+++ MLCSpadalabackprop/MLCSpadalabackprop/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace MLCSpadalabackprop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.makeneuralBtn = new System.Windows.Forms.Button();
+            this.trainBtn = new System.Windows.Forms.Button();
+            this.input1 = new System.Windows.Forms.TextBox();
+            this.input2 = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.testBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openWeights = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // makeneuralBtn
+            // 
+            this.makeneuralBtn.Location = new System.Drawing.Point(12, 12);
+            this.makeneuralBtn.Name = "makeneuralBtn";
+            this.makeneuralBtn.Size = new System.Drawing.Size(101, 23);
+            this.makeneuralBtn.TabIndex = 0;
+            this.makeneuralBtn.Text = "make neural net";
+            this.makeneuralBtn.UseVisualStyleBackColor = true;
+            this.makeneuralBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trainBtn
+            // 
+            this.trainBtn.Location = new System.Drawing.Point(12, 41);
+            this.trainBtn.Name = "trainBtn";
+            this.trainBtn.Size = new System.Drawing.Size(101, 23);
+            this.trainBtn.TabIndex = 1;
+            this.trainBtn.Text = "train network";
+            this.trainBtn.UseVisualStyleBackColor = true;
+            this.trainBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // input1
+            // 
+            this.input1.Location = new System.Drawing.Point(138, 15);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(100, 20);
+            this.input1.TabIndex = 2;
+            // 
+            // input2
+            // 
+            this.input2.Location = new System.Drawing.Point(138, 44);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(100, 20);
+            this.input2.TabIndex = 3;
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(259, 28);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(100, 20);
+            this.output.TabIndex = 4;
+            // 
+            // testBtn
+            // 
+            this.testBtn.Location = new System.Drawing.Point(137, 75);
+            this.testBtn.Name = "testBtn";
+            this.testBtn.Size = new System.Drawing.Size(101, 23);
+            this.testBtn.TabIndex = 5;
+            this.testBtn.Text = "test";
+            this.testBtn.UseVisualStyleBackColor = true;
+            this.testBtn.Click += new System.EventHandler(this.testBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(51, 104);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(101, 23);
+            this.saveBtn.TabIndex = 6;
+            this.saveBtn.Text = "save weights";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // openWeights
+            // 
+            this.openWeights.Location = new System.Drawing.Point(212, 104);
+            this.openWeights.Name = "openWeights";
+            this.openWeights.Size = new System.Drawing.Size(101, 23);
+            this.openWeights.TabIndex = 7;
+            this.openWeights.Text = "open weights";
+            this.openWeights.UseVisualStyleBackColor = true;
+            this.openWeights.Click += new System.EventHandler(this.openWeights_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 138);
+            this.Controls.Add(this.openWeights);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.testBtn);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input2);
+            this.Controls.Add(this.input1);
+            this.Controls.Add(this.trainBtn);
+            this.Controls.Add(this.makeneuralBtn);
+            this.Name = "Form1";
+            this.Text = "AND Function with Backprop";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button makeneuralBtn;
+        private System.Windows.Forms.Button trainBtn;
+        private System.Windows.Forms.TextBox input1;
+        private System.Windows.Forms.TextBox input2;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button testBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button openWeights;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ MLCSpadalabackprop/MLCSpadalabackprop/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Backprop;
+
+namespace MLCSpadalabackprop
+{
+    public partial class Form1 : Form
+    {
+        NeuralNet bp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bp = new NeuralNet(2, 1, 1);    //2 input, 1 hidden, 1 output
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // inputs 1 or 1, 1 or 0, 0 or 1, 0 or o
+            
+            // setInputs are the features
+            // this is an example of supervised network
+
+            for (int x = 0; x < 1000; x++)  //1000 epochs
+            {
+                /* OR function
+                //1 or 1
+                bp.setInputs(0, 1.0);
+                bp.setInputs(1, 1.0);
+                bp.setDesiredOutput(0, 1.0);
+                bp.learn();
+
+                //1 or 0
+                bp.setInputs(0, 1.0);
+                bp.setInputs(1, 0.0);
+                bp.setDesiredOutput(0, 1.0);
+                bp.learn();
+
+                //0 or 1
+                bp.setInputs(0, 0.0);
+                bp.setInputs(1, 1.0);
+                bp.setDesiredOutput(0, 1.0);
+                bp.learn();
+
+                //0 or 0
+                bp.setInputs(0, 0.0);
+                bp.setInputs(1, 0.0);
+                bp.setDesiredOutput(0, 0.0);
+                bp.learn();*/
+
+                //AND function
+                //1 or 1
+                bp.setInputs(0, 1.0);
+                bp.setInputs(1, 1.0);
+                bp.setDesiredOutput(0, 1.0);
+                bp.learn();
+
+                //1 or 0
+                bp.setInputs(0, 1.0);
+                bp.setInputs(1, 0.0);
+                bp.setDesiredOutput(0, 0.0);
+                bp.learn();
+
+                //0 or 1
+                bp.setInputs(0, 0.0);
+                bp.setInputs(1, 1.0);
+                bp.setDesiredOutput(0, 0.0);
+                bp.learn();
+
+                //0 or 0
+                bp.setInputs(0, 0.0);
+                bp.setInputs(1, 0.0);
+                bp.setDesiredOutput(0, 0.0);
+                bp.learn();
+            }
+        }
+
+        private void testBtn_Click(object sender, EventArgs e)
+        {
+            bp.setInputs(0, Convert.ToDouble(input1.Text));
+            bp.setInputs(1, Convert.ToDouble(input2.Text));
+            bp.run();
+
+            output.Text = "" + bp.getOuputData(0);
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            bp.saveWeights(saveFileDialog1.FileName);   
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void openWeights_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            bp.loadWeights(openFileDialog1.FileName);
+        }
+    }
+}
